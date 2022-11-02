67b74f42f595bbca40a43ad4cd7abe135ddf7efb
Domino731 <d.orzechowski18@gmail.com>
2022-11-02 18:50:56 +0100

    setup for pixi

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,50 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
-const mode = process.env.NODE_ENV || "development";
-const target = process.env.NODE_ENV === "production" ? "browserslist" : "web";
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  mode: mode,
-  stats: {
-    children: false,
-  },
-  entry: {
-    index: `./src/index.ts`
-  },
-  plugins: [new MiniCssExtractPlugin()],
-
-  module: {
-    rules: [
-      // typescript compilation into common js
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader",
-        },
-      },
-      // file loader for images
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          }
-        ]
-      },
-      // sass compiler
-      {
-        test: /\.(s[ac]|c)ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
+    // Basic configuration
+    entry: './src/index.ts',
+    // Necessary in order to use source maps and debug directly TypeScript files
+    devtool: 'source-map',
+    module: {
+        rules: [
+            // Necessary in order to use TypeScript
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
         ],
-      },
+    },
+    resolve: {
+        // Alway keep '.js' even though you don't use it.
+        // https://github.com/webpack/webpack-dev-server/issues/720#issuecomment-268470989
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        // This line is VERY important for VS Code debugging to attach properly
+        // Tamper with it at your own risks
+        devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    },
+    plugins: [
+        // No need to write a index.html
+        new HtmlWebpackPlugin(),
+        // Do not accumulate files in ./dist
+        new CleanWebpackPlugin(),
+        // Copy assets to serve them
+        new CopyPlugin([{ from: 'assets', to: 'assets' }]),
     ],
-  },
-  target: target,
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  devtool: "source-map",
-  devServer: {
-    contentBase: path.join(__dirname),
-    publicPath: "/dist/",
-    compress: true,
-    port: 3001,
-    historyApiFallback: true,
-  },
-};
+    devServer: {
+        // webpack-dev-server configuration
+        contentBase: path.join(__dirname, 'dist'),
+        // keep port in sync with VS Code launch.json
+        port: 3000,
+        // Hot-reloading, the sole reason to use webpack here <3
+        hot: true,
+        writeToDisk: true,
+    },
+}
