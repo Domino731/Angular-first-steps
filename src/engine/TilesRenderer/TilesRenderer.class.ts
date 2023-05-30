94f3b8dad38d7e6e1b1b06b8241d8c395d47785a
Domino731 <d.orzechowski18@gmail.com>
2023-05-30 17:46:54 +0200

    feature - tiles - rendering all tiles (black tiles too)

--- src/engine/TilesRenderer/TilesRenderer.class.ts
+++ src/engine/TilesRenderer/TilesRenderer.class.ts
@@ -5,22 +5,20 @@ import {createTilesData} from "./TilesRenderer.utils";
 
 export class TilesRenderer {
     private allTiles: Array<Array<SceneJsonTile>>;
+    private tiles: Array<Tile>;
 
     constructor(sceneTiles: Array<SceneJsonTile>) {
-        this.allTiles = createTilesData(sceneTiles)
+        this.allTiles = createTilesData(sceneTiles);
+        this.tiles = [];
         this.render();
     }
 
     private render(): void {
-        // this.allTiles.forEach(nestedTiles => nestedTiles.forEach(tile => {
-        //     // if (tile.spriteName !== 'Outdoors summer') {
-        //     new Tile(tile);
-        //     // }
-        // }))
-        const tile = new Tile(this.allTiles[0][0]);
-        const til2 = new Tile(this.allTiles[0][1]);
-        console.log(this.allTiles[0])
-        app.stage.addChild(tile);
-        app.stage.addChild(til2)
+        this.allTiles.forEach(nestedTiles => nestedTiles.forEach(tile => {
+            // if (tile.spriteName !== 'Outdoors summer') {
+            const newTile = new Tile(tile);
+            this.tiles.push(newTile);
+            app.stage.addChild(newTile)
+        }))
     }
 }
