eb071d82ef17f42464a4701467247c00073c3729
Domino731 <d.orzechowski18@gmail.com>
2023-07-03 15:44:27 +0200

    feature - player animations

--- src/main/java/inputs/KeyboardInputs.java
+++ src/main/java/inputs/KeyboardInputs.java
@@ -1,64 +1,57 @@
 package inputs;
 
-import Engine2.GamePanel;
-
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import main.GamePanel;
+import static utilz.Constants.Directions.*;
+
 public class KeyboardInputs implements KeyListener {
-    private GamePanel gamePanel;
 
-    public KeyboardInputs(GamePanel gamePanel) {
-        this.gamePanel = gamePanel;
-    }
+	private GamePanel gamePanel;
 
-    @Override
-    public void keyTyped(KeyEvent e) {
+	public KeyboardInputs(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
 
-    }
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        switch (e.getKeyCode()){
-            case KeyEvent.VK_W:
-                System.out.println('W');
-                this.gamePanel.changeYDelta(-5);
-                break;
-            case KeyEvent.VK_S:
-                System.out.println('S');
-                this.gamePanel.changeYDelta(-5);
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			gamePanel.getGame().getPlayer().setUp(false);
+			break;
+		case KeyEvent.VK_A:
+			gamePanel.getGame().getPlayer().setLeft(false);
+			break;
+		case KeyEvent.VK_S:
+			gamePanel.getGame().getPlayer().setDown(false);
+			break;
+		case KeyEvent.VK_D:
+			gamePanel.getGame().getPlayer().setRight(false);
+			break;
+		}
+	}
 
-                break;
-            case KeyEvent.VK_A:
-                System.out.println('A');
-                this.gamePanel.changeXDelta(5);
-                break;
-            case KeyEvent.VK_D:
-                System.out.println('D');
-                this.gamePanel.changeXDelta(5);
-                break;
-        }
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        switch (e.getKeyCode()){
-            case KeyEvent.VK_W:
-//                gamePanel.getEngine().getPlayer().setUp(false);
-//                gamePanel.getEngine().getPlayerClass().setUp(false);
-                break;
-            case KeyEvent.VK_S:
-//                gamePanel.getEngine().getPlayer().setDown(false);
-//                gamePanel.getEngine().getPlayerClass().setDown(false);
-                break;
-            case KeyEvent.VK_A:
-//                gamePanel.getEngine().getPlayer().setLeft(false);
-//                gamePanel.getEngine().getPlayerClass().setLeft(false);
-                break;
-            case KeyEvent.VK_D:
-//                gamePanel.getEngine().getPlayer().setRight(false);
-//                gamePanel.getEngine().getPlayerClass().setRight(false);
-                break;
-        }
-    }
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			gamePanel.getGame().getPlayer().setUp(true);
+			break;
+		case KeyEvent.VK_A:
+			gamePanel.getGame().getPlayer().setLeft(true);
+			break;
+		case KeyEvent.VK_S:
+			gamePanel.getGame().getPlayer().setDown(true);
+			break;
+		case KeyEvent.VK_D:
+			gamePanel.getGame().getPlayer().setRight(true);
+			break;
+		}
+	}
 }
--- src/main/java/inputs/MouseInputs.java
+++ src/main/java/inputs/MouseInputs.java
@@ -1,48 +1,58 @@
 package inputs;
+
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
+import main.GamePanel;
+
 public class MouseInputs implements MouseListener, MouseMotionListener {
-    public MouseInputs(){
 
-    }
+	private GamePanel gamePanel;
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        // BUTTON1 === left click
-        if(e.getButton() == MouseEvent.BUTTON1){
-//            gamePanel.getEngine().getPlayer().setIsAttacking(true);
-        }
-    }
+	public MouseInputs(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
 
-    @Override
-    public void mousePressed(MouseEvent e) {
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void mouseReleased(MouseEvent e) {
+	@Override
+	public void mouseMoved(MouseEvent e) {
 
-    }
+	}
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getButton() == MouseEvent.BUTTON1)
+			gamePanel.getGame().getPlayer().setAttacking(true);
 
-    }
+	}
 
-    @Override
-    public void mouseExited(MouseEvent e) {
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        System.out.println("Mouse dragged");
-    }
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-    @Override
-    public void mouseMoved(MouseEvent e) {
+	}
 
-    }
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
+
 }
