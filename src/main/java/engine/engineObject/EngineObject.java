b1c3b99352f67a5b4820ff6187640546ffdd33be
Domino731 <d.orzechowski18@gmail.com>
2023-07-11 16:53:19 +0200

    feature - rendering objects - EngineObject

--- src/main/java/engine/engineObject/EngineObject.java
+++ src/main/java/engine/engineObject/EngineObject.java
@@ -1,10 +1,19 @@
 package engine.engineObject;
 
+import engine.utils.vectors.Vector2i;
+import engine.utils.vectors.Vector2s;
+
 import java.awt.*;
 
 public abstract class EngineObject implements EngineObjectInterface {
-    public EngineObject(){
+    private Vector2i cords;
+    private Vector2s size;
+    private String name;
 
+    public EngineObject(String name, Vector2i cords, Vector2s size){
+         this.name = name;
+         this.cords = cords;
+         this.size = size;
     }
 
     public void render(Graphics g) {
--- src/main/java/engine/engineObject/EngineObjectInterface.java
+++ src/main/java/engine/engineObject/EngineObjectInterface.java
@@ -1,8 +1,14 @@
 package engine.engineObject;
 
+import engine.utils.vectors.Vector2i;
+import engine.utils.vectors.Vector2s;
+
 import java.awt.*;
 
 public interface EngineObjectInterface {
+    Vector2i cords = null;
+    Vector2s size = null;
+    String name = null;
 
     public void render(Graphics g);
 }
