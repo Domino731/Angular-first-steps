b1188587a914f1805a8f593e37c4e1eb72b21104
Domino731 <d.orzechowski18@gmail.com>
2023-07-17 21:47:29 +0200

    feature - fixed camera

--- Inferno/core/src/screen/PlayScreen.java
+++ Inferno/core/src/screen/PlayScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -22,24 +23,24 @@ public class PlayScreen implements Screen {
     private Texture img;
     private LevelManager levelManager;
 
-    
-    OrthographicCamera camera = new OrthographicCamera();
+    Sprite sprite;
+    OrthographicCamera camera;
+    final  float GAME_WORLD_WIDTH = 100;
+    final float GAME_WORLD_HEIGHT = 100;
+
     public PlayScreen(MyGdxGame game) {
+        sprite = new Sprite(new Texture(Gdx.files.internal("testImg.jpg")));
+        sprite.setSize(GAME_WORLD_WIDTH, GAME_WORLD_HEIGHT);
+        float aspectRatio = (float) Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth();
+
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+
         this.game = game;
         img = new Texture("badlogic.jpg");
-        gameCam = new OrthographicCamera();
-        gameCam.zoom = 0.3F;
-        gamePort = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), gameCam);
-
         gamePort = new FitViewport(1920, 1080, gameCam);
-        int screenWidth = 800;
-        int screenHeight = 600;
         levelManager = new LevelManager();
-        camera.setToOrtho(false, screenWidth, screenHeight);
-
-//        gamePort.update(windowWidth, windowHeight, true);
-
-        gameCam.position.set(0, 0, 0);
+        camera.translate(camera.viewportWidth / 2, camera.viewportHeight / 2);
     }
 
     @Override
@@ -48,20 +49,16 @@ public class PlayScreen implements Screen {
     }
 
     private void update(float delta) {
-        if(Gdx.input.isTouched()){
-            System.out.println(1);
-            gameCam.position.x += 100 * delta;
-        }
-
-        gameCam.update();
     }
 
     @Override
     public void render(float delta) {
         update(delta);
         ScreenUtils.clear(0, 0, 0, 1);
-        game.batch.setProjectionMatrix(camera.combined);
+        camera.update();
         game.batch.begin();
+        game.batch.setProjectionMatrix(camera.combined);
+        sprite.draw(game.batch);
         levelManager.render(game.batch);
         game.batch.end();
     }
