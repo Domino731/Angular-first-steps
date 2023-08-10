1d7a81d0e01d133840123e4db64c6e0ebd95fb28
Domino731 <d.orzechowski18@gmail.com>
2023-08-10 21:38:51 +0200

    refactor - EngineLog class - logs improved

--- Inferno/core/src/utils/EngineLog.java
+++ Inferno/core/src/utils/EngineLog.java
@@ -1,27 +1,43 @@
 package utils;
 
+/**
+ * UTILITY CLASS - contains methods for game logs - success, default, alerts...
+ */
 public class EngineLog {
-    public static void print(String text){
-        System.out.println("Engine log: " + text);
+    // ANSI escape code for yellow text
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    // ANSI escape code for green text
+    private static final String ANSI_GREEN = "\u001B[32m";
+    // ANSI escape code for red text
+    private static final String ANSI_RED = "\u001B[31m";
+    // ANSI escape code to reset text color
+    private static final String ANSI_RESET = "\u001B[0m";
+
+    public static void print(String message) {
+        System.out.println("Engine log: " + message);
     }
 
-    public static void success(String text) {
-        System.out.println("Engine log.success: " + text);
+    public static void success(String message) {
+        System.out.println(ANSI_GREEN + message + ANSI_RESET);
     }
 
-    public static void error(String text) {
-        System.out.println("Engine log.error: " + text);
+    public static void error(String message) {
+        System.out.println(ANSI_RED + message + ANSI_RESET);
     }
 
-    public static void alert(String text) {
-        System.out.println("Engine log.alert: " + text);
+    public static void alert(String message) {
+        System.out.println(ANSI_YELLOW + "Engine log.alert: " + message + ANSI_RESET);
     }
 
     public static void classSuccess(String text) {
-        System.out.println("Engine log.success: " + text + " class created");
+        System.out.println(ANSI_GREEN + "Engine log.success: " + text + " class created" + ANSI_RESET);
     }
 
-    public static void resourceError(String text) {
-        System.out.println("Engine log.resource error: failed to load " + text + " resource");
+    public static void resourceSuccess(String resourceSrc) {
+        System.out.println(ANSI_GREEN + "Engine log.resource success: load " + resourceSrc + ANSI_RESET);
+    }
+
+    public static void resourceError(String resourceSrc) {
+        System.out.println(ANSI_RED + "Engine log.resource error: failed to load " + resourceSrc + ANSI_RESET);
     }
 }
