baba3c96a413c9bc7c8b2e9e216ff76d02835578
Domino731 <d.orzechowski18@gmail.com>
2023-08-13 18:06:11 +0200

    feature - player harvest - right axe animation data fixed indices

--- Inferno/core/src/game/entities/player/tools/toolsConstants.java
+++ Inferno/core/src/game/entities/player/tools/toolsConstants.java
@@ -6,21 +6,21 @@ public class toolsConstants {
 
     private static byte[][] createRightAxeAnimation() {
         byte[][] rightAxeAnimation = new byte[5][3];
-        rightAxeAnimation[0][1] = -8;
-        rightAxeAnimation[0][2] = 0;
-        rightAxeAnimation[0][3] = 15;
-        rightAxeAnimation[1][1] = 5;
-        rightAxeAnimation[1][2] = 0;
-        rightAxeAnimation[1][3] = -15;
-        rightAxeAnimation[2][1] = 16;
-        rightAxeAnimation[2][2] = -5;
-        rightAxeAnimation[2][3] = -45;
-        rightAxeAnimation[3][1] = 21;
-        rightAxeAnimation[3][2] = -23;
-        rightAxeAnimation[3][3] = -105;
-        rightAxeAnimation[4][1] = 21;
-        rightAxeAnimation[4][2] = -25;
-        rightAxeAnimation[4][3] = -105;
+        rightAxeAnimation[0][0] = -8;
+        rightAxeAnimation[0][1] = 0;
+        rightAxeAnimation[0][2] = 15;
+        rightAxeAnimation[1][0] = 5;
+        rightAxeAnimation[1][1] = 0;
+        rightAxeAnimation[1][2] = -15;
+        rightAxeAnimation[2][0] = 16;
+        rightAxeAnimation[2][1] = -5;
+        rightAxeAnimation[2][2] = -45;
+        rightAxeAnimation[3][0] = 21;
+        rightAxeAnimation[3][1] = -23;
+        rightAxeAnimation[3][2] = -105;
+        rightAxeAnimation[4][0] = 21;
+        rightAxeAnimation[4][1] = -25;
+        rightAxeAnimation[4][2] = -105;
 
         return rightAxeAnimation;
     }
