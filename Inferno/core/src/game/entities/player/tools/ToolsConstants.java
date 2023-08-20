dd080893f16cce7fb9d1d9f0359e944f1028979f
Domino731 <d.orzechowski18@gmail.com>
2023-08-20 16:04:01 +0200

    feature - player animations - animation data for right

--- Inferno/core/src/game/entities/player/tools/ToolsConstants.java
+++ Inferno/core/src/game/entities/player/tools/ToolsConstants.java
@@ -5,59 +5,56 @@ import utils.Direction;
 public class ToolsConstants {
     public static final byte[][] rightAxeAnimation = createRightAxeAnimation();
     public static final byte[][][] animations = createToolAnimationArray();
-
-    /**
-     * 0 - x offset
-     * 1 - y offset
-     * 3 - rotation angle
-     */
+    private static final byte xIndex = 0;
+    private static final byte yIndex = 1;
+    private static final byte rotateIndex = 2;
 
     private static byte[][] createRightAxeAnimation() {
         byte[][] rightAxeAnimation = new byte[5][3];
-        rightAxeAnimation[0][0] = -8;
-        rightAxeAnimation[0][1] = -2;
-        rightAxeAnimation[0][2] = 15;
+        rightAxeAnimation[0][xIndex] = -8;
+        rightAxeAnimation[0][yIndex] = -2;
+        rightAxeAnimation[0][rotateIndex] = 15;
 
-        rightAxeAnimation[1][0] = 5;
-        rightAxeAnimation[1][1] = -1;
-        rightAxeAnimation[1][2] = -15;
+        rightAxeAnimation[1][xIndex] = 5;
+        rightAxeAnimation[1][yIndex] = -1;
+        rightAxeAnimation[1][rotateIndex] = -15;
 
-        rightAxeAnimation[2][0] = 16;
-        rightAxeAnimation[2][1] = -6;
-        rightAxeAnimation[2][2] = -45;
+        rightAxeAnimation[2][xIndex] = 16;
+        rightAxeAnimation[2][yIndex] = -6;
+        rightAxeAnimation[2][rotateIndex] = -45;
 
-        rightAxeAnimation[3][0] = 21;
-        rightAxeAnimation[3][1] = -24;
-        rightAxeAnimation[3][2] = -105;
+        rightAxeAnimation[3][xIndex] = 21;
+        rightAxeAnimation[3][yIndex] = -24;
+        rightAxeAnimation[3][rotateIndex] = -105;
 
-        rightAxeAnimation[4][0] = 21;
-        rightAxeAnimation[4][1] = -26;
-        rightAxeAnimation[4][2] = -105;
+        rightAxeAnimation[4][xIndex] = 21;
+        rightAxeAnimation[4][yIndex] = -26;
+        rightAxeAnimation[4][rotateIndex] = -105;
 
         return rightAxeAnimation;
     }
 
     private static byte[][] createLeftAxeAnimation() {
         byte[][] rightAxeAnimation = new byte[5][3];
-        rightAxeAnimation[0][0] = 8;
-        rightAxeAnimation[0][1] = -2;
-        rightAxeAnimation[0][2] = -15;
+        rightAxeAnimation[0][xIndex] = 8;
+        rightAxeAnimation[0][yIndex] = -2;
+        rightAxeAnimation[0][rotateIndex] = -15;
 
-        rightAxeAnimation[1][0] = -5;
-        rightAxeAnimation[1][1] = -1;
-        rightAxeAnimation[1][2] = 15;
+        rightAxeAnimation[1][xIndex] = -5;
+        rightAxeAnimation[1][yIndex] = -1;
+        rightAxeAnimation[1][rotateIndex] = 15;
 
-        rightAxeAnimation[2][0] = -16;
-        rightAxeAnimation[2][1] = -6;
-        rightAxeAnimation[2][2] = 45;
+        rightAxeAnimation[2][xIndex] = -16;
+        rightAxeAnimation[2][yIndex] = -6;
+        rightAxeAnimation[2][rotateIndex] = 45;
 
-        rightAxeAnimation[3][0] = -21;
-        rightAxeAnimation[3][1] = -24;
-        rightAxeAnimation[3][2] = 105;
+        rightAxeAnimation[3][xIndex] = -21;
+        rightAxeAnimation[3][yIndex] = -24;
+        rightAxeAnimation[3][rotateIndex] = 105;
 
-        rightAxeAnimation[4][0] = -21;
-        rightAxeAnimation[4][1] = -26;
-        rightAxeAnimation[4][2] = 105;
+        rightAxeAnimation[4][xIndex] = -21;
+        rightAxeAnimation[4][yIndex] = -26;
+        rightAxeAnimation[4][rotateIndex] = 105;
 
         return rightAxeAnimation;
     }
