dbb406584f4c8483d77e80b536bef6b3575d9fb7
Domino731 <d.orzechowski18@gmail.com>
2023-08-26 11:03:15 +0200

    feature - player animations - harvest weed right

--- Inferno/core/src/game/entities/player/PlayerAnimationsTest.java
+++ Inferno/core/src/game/entities/player/PlayerAnimationsTest.java
@@ -10,6 +10,8 @@ public class PlayerAnimationsTest {
     public TextureRegion[][] bodyTextures;
     public TextureRegion[][] armsTextures;
     TextureRegion weaponTexture = loadWeaponTexture();
+    public final byte weaponXOrigin = 16 / 2;
+    public final byte weaponYOrigin = 16 / 2;
 
     public PlayerAnimationsTest(Player player) {
         this.player = player;
@@ -19,23 +21,33 @@ public class PlayerAnimationsTest {
 
     private TextureRegion loadWeaponTexture() {
         Texture txt = new Texture(Urls.weapons);
-        return new TextureRegion(txt, 0, 0, 16, 16);
+        return new TextureRegion(txt, 16 * 7, 16 * 5, 16, 16);
     }
 
     public void harvestWeedRight(SpriteBatch sb) {
-        sb.draw(bodyTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][0], player.finalPosition.x - 25, player.finalPosition.y, 16, 32);
-        sb.draw(armsTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][0], player.finalPosition.x - 25, player.finalPosition.y, 16, 32);
-        sb.draw(weaponTexture, player.finalPosition.x - 25, player.finalPosition.y, 16, 16);
-        sb.draw(bodyTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][1], player.finalPosition.x, player.finalPosition.y, 16, 32);
-        sb.draw(armsTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][1], player.finalPosition.x, player.finalPosition.y, 16, 32);
+//        sb.draw(weaponTexture, (player.finalPosition.x - 25) + 3, (player.finalPosition.y + 20) + 4, weaponXOrigin, weaponYOrigin, 16, 16, 1, 1, 40);
+//        sb.draw(bodyTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][0], player.finalPosition.x - 25, player.finalPosition.y, 16, 32);
+//        sb.draw(armsTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][0], player.finalPosition.x - 25, player.finalPosition.y, 16, 32);
+//
+//        sb.draw(weaponTexture, (player.finalPosition.x) + 13, (player.finalPosition.y + 20) - 4, weaponXOrigin, weaponYOrigin, 16, 16, 1, 1, 0);
+//        sb.draw(bodyTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][1], player.finalPosition.x, player.finalPosition.y, 16, 32);
+//        sb.draw(armsTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][1], player.finalPosition.x, player.finalPosition.y, 16, 32);
 
-        sb.draw(bodyTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][2], player.finalPosition.x + 25, player.finalPosition.y, 16, 32);
-        sb.draw(armsTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][2], player.finalPosition.x + 25, player.finalPosition.y, 16, 32);
+//        sb.draw(weaponTexture, (player.finalPosition.x + 25) + 17, (player.finalPosition.y + 3), weaponXOrigin, weaponYOrigin, 16, 16, 1, 1, -50);
+//        sb.draw(bodyTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][2], player.finalPosition.x + 25, player.finalPosition.y, 16, 32);
+//        sb.draw(armsTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][2], player.finalPosition.x + 25, player.finalPosition.y, 16, 32);
 
-        sb.draw(bodyTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][3], player.finalPosition.x + 50, player.finalPosition.y, 16, 32);
-        sb.draw(armsTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][3], player.finalPosition.x + 50, player.finalPosition.y, 16, 32);
+//        sb.draw(weaponTexture, (player.finalPosition.x + 50) + 14, (player.finalPosition.y - 6), weaponXOrigin, weaponYOrigin, 16, 16, 1, 1, -90);
+//        sb.draw(bodyTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][3], player.finalPosition.x + 50, player.finalPosition.y, 16, 32);
+//        sb.draw(armsTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][3], player.finalPosition.x + 50, player.finalPosition.y, 16, 32);
+//
 
-        sb.draw(bodyTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][4], player.finalPosition.x + 75, player.finalPosition.y, 16, 32);
-        sb.draw(armsTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][4], player.finalPosition.x + 75, player.finalPosition.y, 16, 32);
+//        sb.draw(bodyTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][4], player.finalPosition.x + 75, player.finalPosition.y, 16, 32);
+//        sb.draw(armsTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][4], player.finalPosition.x + 75, player.finalPosition.y, 16, 32);
+//        sb.draw(weaponTexture, (player.finalPosition.x + 75) + 2, (player.finalPosition.y - 10), weaponXOrigin, weaponYOrigin, 16, 16, 1, 1, -130);
+
+        sb.draw(bodyTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][5], player.finalPosition.x + 75, player.finalPosition.y, 16, 32);
+        sb.draw(armsTextures[PlayerTextures.STATE_HARVEST_WEED_RIGHT][5], player.finalPosition.x + 75, player.finalPosition.y, 16, 32);
+        sb.draw(weaponTexture, (player.finalPosition.x + 75) - 6, (player.finalPosition.y - 11), weaponXOrigin, weaponYOrigin, 16, 16, 1, 1, -146);
     }
 }
