16d27b2319fd62672d5551ec1398da93f850a11a
Domino731 <d.orzechowski18@gmail.com>
2023-09-03 11:48:04 +0200

    refactor - player animations - running & running with item offset for shirt, hat, hair

--- Inferno/core/src/game/entities/player/animations/config/HatOffset.java
+++ Inferno/core/src/game/entities/player/animations/config/HatOffset.java
@@ -5,8 +5,8 @@ import game.entities.player.PlayerTextures;
 import static game.entities.player.PlayerTextures.ANIMATION_AMOUNT;
 import static game.entities.player.PlayerTextures.MAX_ANIMATION_FRAMES;
 
-public class HairOffset {
-    public static final byte[][] hairOffset = createOffsets();
+public class HatOffset {
+    public static final byte[][] shirtOffset = createOffsets();
 
     private static byte[][] createOffsets() {
         byte[][] data = new byte[ANIMATION_AMOUNT][MAX_ANIMATION_FRAMES];
@@ -18,15 +18,14 @@ public class HairOffset {
         data[PlayerTextures.STATE_IDLE_LEFT] = new byte[]{-2};
         // RUNNING
         data[PlayerTextures.STATE_RUNNING_UP] = new byte[]{-2};
-        data[PlayerTextures.STATE_RUNNING_RIGHT] = new byte[]{-4, -5, -4, -3, -4, -5, -4, -3};
+        data[PlayerTextures.STATE_RUNNING_RIGHT] = new byte[]{0, -1, 0, 1, 0, -1, 0, 1};
         data[PlayerTextures.STATE_RUNNING_DOWN] = new byte[]{-2};
-        data[PlayerTextures.STATE_RUNNING_LEFT] = new byte[]{-4, -5, -4, -3, -4, -5, -4, -3};
-
+        data[PlayerTextures.STATE_RUNNING_LEFT] = new byte[]{0, -1, 0, 1, 0, -1, 0, 1};
         // RUNNING WITH ITEM
         data[PlayerTextures.STATE_RUNNING_ITEM_UP] = new byte[]{-2};
-        data[PlayerTextures.STATE_RUNNING_ITEM_RIGHT] = new byte[]{-4, -5, -4, -3, -4, -5, -4, -3};
+        data[PlayerTextures.STATE_RUNNING_ITEM_RIGHT] = new byte[]{0, -1, 0, 1, 0, -1, 0, 1};
         data[PlayerTextures.STATE_RUNNING_ITEM_DOWN] = new byte[]{-2};
-        data[PlayerTextures.STATE_RUNNING_ITEM_LEFT] = new byte[]{-4, -5, -4, -3, -4, -5, -4, -3};
+        data[PlayerTextures.STATE_RUNNING_ITEM_LEFT] = new byte[]{0, -1, 0, 1, 0, -1, 0, 1};
 
         return data;
     }
