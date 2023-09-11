006438f10d7b17593abbe520435d88bba2660314
Domino731 <d.orzechowski18@gmail.com>
2023-09-11 16:11:20 +0200

    feature - resources - initial ground item radius

--- Inferno/core/src/engine/actors/groundItem/GroundItem.java
+++ Inferno/core/src/engine/actors/groundItem/GroundItem.java
@@ -11,24 +11,27 @@ public class GroundItem {
     private TextureRegion txt;
     private ArrayList<Vector<Integer>> positions = new ArrayList<>();
     private int positionIndex = 0;
+    private boolean isAvailableToPick = false;
 
     public GroundItem(int positionX, int positionY, TextureRegion txt) {
         position = new Vector<>(positionX, positionY);
         this.txt = txt;
         // f(x) = x2
-        positions.add(new Vector<>(position.x + -3, position.y + 9));
-        positions.add(new Vector<>(position.x + -2, position.y + 4));
-        positions.add(new Vector<>(position.x + -1, position.y + 1));
+        positions.add(new Vector<>(position.x - 3, position.y + 9));
+        positions.add(new Vector<>(position.x - 2, position.y + 4));
+        positions.add(new Vector<>(position.x - 1, position.y + 1));
         positions.add(new Vector<>(position.x + 0, position.y + 0));
-        positions.add(new Vector<>(position.x + -1, position.y + 1));
-        positions.add(new Vector<>(position.x + -2, position.y + 4));
-        positions.add(new Vector<>(position.x + -3, position.y + 9));
+        positions.add(new Vector<>(position.x + 1, position.y + 1));
+        positions.add(new Vector<>(position.x + 2, position.y + 4));
+        positions.add(new Vector<>(position.x + 3, position.y + 9));
     }
 
     public void update() {
         if (positionIndex < positions.size() - 1) {
             positionIndex++;
             position = positions.get(positionIndex);
+        } else {
+            isAvailableToPick = true;
         }
     }
 
